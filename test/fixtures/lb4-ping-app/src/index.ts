fcc72ce4e27d5bc9f92ad3ed25e57b43dbc30088
Raymond Feng <enjoyjava@gmail.com>
2019-03-11 20:35:59 -0700

    chore: update copyright headers

--- test/fixtures/lb4-ping-app/src/index.ts
+++ test/fixtures/lb4-ping-app/src/index.ts
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2019. All Rights Reserved.
+// Node module: loopback-connector-openapi
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 import {PingApplication} from './application';
 import {ApplicationConfig} from '@loopback/core';
 
